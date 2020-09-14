b910bd00b560da582156645642aab57f3b29af26
kopz9m <piz199640@gmail.com>
2020-09-14 11:48:25 +0900

    publish added

--- src/main/java/life/majiang/community/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/community/controller/IndexController.java
@@ -4,9 +4,7 @@ import life.majiang.community.community.Model.User;
 import life.majiang.community.community.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
