fa8c13b08f225c95661199b3387914f867b27b9c
kopz9m <piz199640@gmail.com>
2020-09-23 22:57:57 +0900

    multiple pages

--- src/main/java/life/majiang/community/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/community/controller/IndexController.java
@@ -1,6 +1,7 @@
 package life.majiang.community.community.controller;
 
 import life.majiang.community.community.Model.User;
+import life.majiang.community.community.dto.PaginationDTO;
 import life.majiang.community.community.dto.QuestionDTO;
 import life.majiang.community.community.mapper.UserMapper;
 import life.majiang.community.community.service.QuestionService;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -20,7 +22,11 @@ public class IndexController {
     @Autowired
     private QuestionService questionService;
     @GetMapping("/")
-    public String index(HttpServletRequest request, Model model){
+    public String index(HttpServletRequest request,
+                        Model model,
+                        @RequestParam(name = "page",defaultValue = "1") Integer page,
+                        @RequestParam(name = "size",defaultValue = "5") Integer size
+                        ){
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0)
         for (Cookie cookie : cookies) {
@@ -33,8 +39,8 @@ public class IndexController {
                 break;
             }
         }
-        List<QuestionDTO> questionList = questionService.list();
-        model.addAttribute("questions",questionList);
+        PaginationDTO pagination = questionService.list(page,size);
+        model.addAttribute("pagination",pagination);
         return "index";
     }
 }
--- src/main/java/life/majiang/community/community/service/QuestionService.java
+++ src/main/java/life/majiang/community/community/service/QuestionService.java
@@ -1,12 +1,11 @@
 package life.majiang.community.community.service;
 
-import com.fasterxml.jackson.databind.util.BeanUtil;
 import life.majiang.community.community.Model.Question;
 import life.majiang.community.community.Model.User;
+import life.majiang.community.community.dto.PaginationDTO;
 import life.majiang.community.community.dto.QuestionDTO;
 import life.majiang.community.community.mapper.QuestionMapper;
 import life.majiang.community.community.mapper.UserMapper;
-import lombok.Data;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,10 +19,13 @@ public class QuestionService {
     private UserMapper userMapper;
     @Autowired
     private QuestionMapper questionMapper;
-    public List<QuestionDTO> list() {
-        List<Question> questions = questionMapper.list();
+    public PaginationDTO list(Integer page, Integer size) {
+
+        Integer offset = size * (page - 1);
+        List<Question> questions = questionMapper.list(offset,size);
         List<QuestionDTO> questionDTOList= new ArrayList<>();
 
+        PaginationDTO paginationDTO = new PaginationDTO();
         for (Question question : questions) {
             User user = userMapper.findById(question.getCreator());
             QuestionDTO questionDTO = new QuestionDTO();
@@ -31,8 +33,11 @@ public class QuestionService {
             questionDTO.setUser(user);
             questionDTOList.add(questionDTO);
         }
+        paginationDTO.setQuestions(questionDTOList);
+        Integer totalCount = questionMapper.count();
+        paginationDTO.setPagination(totalCount,page,size);
 
-        return questionDTOList;
+        return paginationDTO;
 
     }
 }
