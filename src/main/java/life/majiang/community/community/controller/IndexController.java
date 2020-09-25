9607c360bf15be2867b327d4a189490fe159785f
kopz9m <piz199640@gmail.com>
2020-09-25 21:50:10 +0900

    profile page

--- src/main/java/life/majiang/community/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/community/controller/IndexController.java
@@ -28,15 +28,16 @@ public class IndexController {
                         @RequestParam(name = "size",defaultValue = "2") Integer size
                         ){
         Cookie[] cookies = request.getCookies();
-        if (cookies != null && cookies.length != 0)
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals("token")){
-                String token = cookie.getValue();
-                User user = userMapper.findByToken(token);
-                if (user != null){
-                    request.getSession().setAttribute("user",user);
+        if (cookies != null && cookies.length != 0){
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")){
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    if (user != null){
+                        request.getSession().setAttribute("user",user);
+                    }
+                    break;
                 }
-                break;
             }
         }
         PaginationDTO pagination = questionService.list(page,size);
